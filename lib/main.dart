6f19414da4040fc1d5d7a1252166d78f6bbc5386
Leandre <leajapo@gmail.com>
2019-10-21 16:58:29 +0200

    added raised buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,40 @@
 import 'package:flutter/material.dart';
 
-void main(List<String> args) {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('question answered');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('My Quiz App'),
+            title: Center(
+          child: Text('My Quiz App'),
+        )),
+        body: Column(
+          children: <Widget>[
+            Text('Questions'),
+            RaisedButton(
+              elevation: 10,
+              child: Text('Question 1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              elevation: 10,
+              child: Text('Question 2'),
+              onPressed: () => print('question 2 answered'),
+            ),
+            RaisedButton(
+              elevation: 10,
+              child: Text('Question 3'),
+              onPressed: () => print('question 3 answered'),
+            )
+          ],
         ),
-        body: Text('Damn right'),
       ),
     );
   }
